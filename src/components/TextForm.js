4dc5ddadcacc90fd8fac70b91b6029643620eb34
tpt7k <115703163+tpt7k@users.noreply.github.com>
2022-10-13 21:00:37 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,18 +3,25 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
+        undoTextHistory.push(text);
+    updateUndoTextHistory(undoTextHistory);
         let newText = text.toUpperCase();
         setText(newText)
         props.showAlert("Converted to uppercase!", "success");
     }
 
     const handleLoClick = ()=>{ 
+        undoTextHistory.push(text);
+    updateUndoTextHistory(undoTextHistory);
         let newText = text.toLowerCase();
+        
         setText(newText)
         props.showAlert("Converted to lowercase!", "success");
     }
 
     const handleClearClick = ()=>{ 
+        undoTextHistory.push(text);
+    updateUndoTextHistory(undoTextHistory);
         let newText = '';
         setText(newText);
         props.showAlert("Text Cleared!", "success");
@@ -26,12 +33,16 @@ export default function TextForm(props) {
 
     // Credits: A
     const handleCopy = () => {
+        undoTextHistory.push(text);
+    updateUndoTextHistory(undoTextHistory);
         navigator.clipboard.writeText(text); 
         props.showAlert("Copied to Clipboard!", "success");
     }
 
     // Credits: Coding Wala
     const handleExtraSpaces = () => {
+        undoTextHistory.push(text);
+    updateUndoTextHistory(undoTextHistory);
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
         props.showAlert("Extra spaces removed!", "success");
@@ -47,6 +58,18 @@ export default function TextForm(props) {
             alert("speech synthesis not supported ")
         }
     }
+      const handleRedoText = () => {
+    undoTextHistory.push(text);
+    updateUndoTextHistory(undoTextHistory);
+    const lastText = redoTextHistory[redoTextHistory.length - 1];
+    redoTextHistory.pop();
+    updateRedoTextHistory(redoTextHistory);
+    setText(lastText);
+    props.showAlert("Successfully recovered your last text!", "success");
+  };
+
+  const [redoTextHistory, updateRedoTextHistory] = useState([]);
+  const [undoTextHistory, updateUndoTextHistory] = useState([]);
 
     const [text, setText] = useState(''); 
     // text = "new text"; // Wrong way to change the state
@@ -64,6 +87,14 @@ export default function TextForm(props) {
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleReadAloud}>Read Aloud</button>
+            <button
+          disabled={redoTextHistory.length === 0}
+          className="custom-button mx-1 my-1"
+          type="submit"
+          onClick={handleRedoText}
+        >
+          Redo Text
+        </button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
